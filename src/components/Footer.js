4f03d6a9e4789e29fc60f0dcd757965b545bc55e
Aniket <aniketsthorat11@gmail.com>
2023-04-16 12:26:34 +0530

    changed Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer class="container-fluid text-center">
-            <a href="#" title="To Top">
+            <a href="#myPage" title="To Top">
                 <span class="glyphicon glyphicon-chevron-up"></span>
             </a>
-            <p>Theme Made By <a href="#" title="Visit w3schools">Abid Akram</a></p>
+            {/* <p>Theme Made By <a href="#" title="Visit w3schools">Abid Akram</a></p> */}
         </footer>
     )
 }

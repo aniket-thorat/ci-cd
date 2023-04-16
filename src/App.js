5061849e712ef5e9107446e68f3b809fc1a6ce13
Aniket <aniketsthorat11@gmail.com>
2023-04-16 12:06:47 +0530

    created full site

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Services from './components/Services';
+import Portfolio from './components/Portfolio';
+import Testimonial from './components/Testimonial';
+import Pricing from './components/Pricing';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>This is h1 tag</h1>
-        <h2>This is h2 tag</h2>
-        <h3>This is h3 tag</h3>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Portfolio />
+      <Testimonial />
+      <Pricing />
+      <Contact />
+      <Footer />
     </div>
   );
 }

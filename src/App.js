7d5b1a43b256978bb8bf439734ca2b07bfa23d9b
Aniket <aniketsthorat11@gmail.com>
2023-04-16 11:17:37 +0530

    changed App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>This is h1 tag</h1>
+        <h2>This is h2 tag</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
